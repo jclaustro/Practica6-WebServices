7958fb750cfc3f8bf2b1718c4508d919812cb511
JAVIER CLAUSTRO <deadnot9@gmail.com>
2018-09-25 19:39:38 -0500

    Initial Commit

---
+++ Practica6-WebServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica6-WebServices
+//
+//  Created by JAVIER CLAUSTRO on 25/09/18.
+//  Copyright © 2018 JAVIER CLAUSTRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Practica6-WebServicesTests/Practica6_WebServicesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Practica6_WebServicesTests.swift
+//  Practica6-WebServicesTests
+//
+//  Created by JAVIER CLAUSTRO on 25/09/18.
+//  Copyright © 2018 JAVIER CLAUSTRO. All rights reserved.
+//
+
+import XCTest
+@testable import Practica6_WebServices
+
+class Practica6_WebServicesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
